9fb959864b0194daecedb7fc0a7de4863514bdea
01as 1 <01as 1>
2017-02-02 17:21:47 -0600

    Cleaned it up and added escape function

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import caesar
+import cgi
+
+def build_page(textcontent):
+    styles = "<style>.numb{width:40px}</style>"
+    header = "<head>" + styles + "</head>"
+    outpt = header + "<p>Enter a message to encrypt:</p><form method='post'><textarea name='massage'>" + textcontent + "</textarea><p>Rotate by:<input type='number' name='bread' class='numb'></p><input type='submit'/></form>"
+    return outpt
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.write(build_page(""))
 
-        content = "Hello World"
-        encryptd = caesar.encrypt(content,14)
-        outpt = "<form><textarea>" + encryptd + "</textarea><br><input type='submit'/></form>"
-
-        self.response.write(outpt)
+    def post(self):
+        ms = self.request.get("massage")
+        rt = int(self.request.get("bread"))
+        enc = caesar.encrypt(ms,rt)
+        esc_enc = cgi.escape(enc)
+        self.response.write(build_page(esc_enc))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
